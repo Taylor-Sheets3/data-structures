75296988c17431ce131814748ee7e10c45bf96bc
Taylor <taylor.sheets3@gmail.com>
2022-09-10 12:57:28 -0700

    binary search tree started

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,36 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(binaryTreeMethods);
+  value = value;
+  newTree.left = {};
+  newTree.right = {};
+  return newTree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value){
+  //RECURSION!!!!!
+  // coditional to not add value if value if value is present
+  // if value is less then this value
+    // create new BST in left property
+  // if value is greater than this value
+    //create new BST in right property
+  // this.right[value] = BinarySearchTree(value);
+
+};
+
+binaryTreeMethods.contains = function(value){
+
+};
+
+binaryTreeMethods.depthFirstLog = function(cb){
+
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ var bST = BinarySearchTree(5);
+
+ bST.insert(6)
